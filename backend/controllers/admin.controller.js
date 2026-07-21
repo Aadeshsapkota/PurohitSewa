704ead357927629deb62ed7dcdbfae67bb558dfd
Ankit Acharya <ankitacharya1217@gmail.com>
2026-07-21 21:36:54 +0545

    Add Auth and Admi

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -3,7 +3,7 @@ import prisma from "../utils/prisma.js"
 export const getStats = async (req, res) =>{
     try{
         const bookings= await prisma.booking.count()
-        console.log(bookings);
+        
 
     }
  catch (err) {
